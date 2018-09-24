cb930a55c47d66f7b3df23212db9933ea9ce1179
shtahvildary <shtahvildary@gmail.com>
2018-09-24 16:09:47 +0330

    small changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './styles/App.css';
 import InfoPlayer from './PlayerPage'
 import "./styles/scss/video-react.scss"
 
--- src/PlayerPage.js
+++ src/PlayerPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './styles/App.css';
 import Player from './components/Player';
 import Menu from './components/Menu'
 
@@ -9,7 +9,7 @@ class PlayerPage extends Component {
     this.state = {
       // channels: [{ name: 'شبکه یک', address: "http://192.168.1.3:8000/out.m3u8" }, { name: 'شبکه ورزش', address: "http://192.168.1.3:8000/out2.m3u8" }],
       // channels: [{ name: 'شبکه یک', address: "http://192.168.0.116:8000/out.m3u8" }, { name: 'شبکه ورزش', address: "http://192.168.0.116:8000/out2.m3u8" }],
-      channels: [{ name: 'شبکه یک', address: "http://172.16.16.153:8000/tv1.m3u8" }, { name: 'شبکه ورزش', address: "http://172.16.16.153:8000/varzesh.m3u8" }],
+      channels: [{ name: 'شبکه یک', address: "http://172.16.16.153:8000/tv1.m3u8" }, { name: 'شبکه ورزش', address: "http://172.16.16.153:8000/varzesh.m3u8" },{ name: 'موزاییک', address: "http://172.16.16.153:8000/mosaic.m3u8" }],
       // src: 'http://192.168.1.3:8000/out.m3u8'
       // src: 'http://192.168.0.116:8000/out.m3u8'
       src: 'http://172.16.16.153:8000/tv1.m3u8'
