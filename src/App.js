356c7f65294a4d1cf2915ba26f1e54304badab36
shtahvildary <shtahvildary@gmail.com>
2019-04-23 14:54:17 +0430

    change ips

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './styles/App.css';
-import InfoPlayer from './PlayerPage'
+// import InfoPlayer from './PlayerPage'
+import ArchivePlayer from './ArchivePlayer'
 import "./styles/scss/video-react.scss"
 global.serverAddress = "http://localhost:5000/api";
 
@@ -8,7 +9,8 @@ class App extends Component {
   render() {
     return (
       <div className="App" dir='rtl'>
-        <InfoPlayer/>
+        <ArchivePlayer/>
+        {/* <InfoPlayer/> */}
       </div>
     );
   }
